b2b273da26f2d6236c3db769572197f60734a645
RM349626@fiap.com.br <RM349626@fiap.com.br>
2024-03-11 21:52:21 -0300

    initial commit

--- src/main/java/com/fiap/hospitality/SpringDocConfigurations.java
+++ src/main/java/com/fiap/hospitality/SpringDocConfigurations.java
@@ -1,14 +1,10 @@
 package com.fiap.hospitality;
 
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Hospitality Simple", version = "0.0.1", description = "The best place to you", license = @License(name = "MIT License", url = "https://github.com/alexgrds/hospitality-graduate")),
